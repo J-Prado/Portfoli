95d89de12032ec5d5de5a7fe57a58962b1155e02
Juan Prado <juan_prado_rojas@outlook.com>
2022-07-06 11:00:47 -0400

    New Deploy

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
-
+import gsap from 'gsap-trial';
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 import LogoS from '../../../assets/images/logo-s.png';
 import './index.scss';
 
@@ -9,6 +10,8 @@ const Logo = () => {
   const solidLogoRef = useRef();
 
   useEffect(() => {
+    gsap.registerPlugin(DrawSVGPlugin);
+
     gsap
       .timeline()
       .to(bgRef.current, {
