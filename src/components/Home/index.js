0d560909c88c8d267bf57dcdeac49d180fdb02c1
Juan Prado <juan_prado_rojas@outlook.com>
2022-07-12 22:34:10 -0400

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -64,7 +64,7 @@ const Home = () => {
           </h1>
           <h2>Full-Stack Developer || JavaScript Developer || Engineer</h2>
           <a
-            href="https://drive.google.com/file/d/1p6jzs4VzKePDvkXhaQmIBdfkFC47JTfX/view?usp=sharing"
+            href="https://drive.google.com/file/d/17eUXmjLb40hN484bj0JcYzrI231Slnx3/view?usp=sharing"
             className="flat-button"
           >
             DOWNLOAD CV
